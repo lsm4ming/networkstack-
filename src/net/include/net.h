0e539d9e4ad03d4302ed9b9d3940b5dc1be7bb2e
lsm <487005831@qq.com>
2024-07-02 00:59:05 +0800

    初始化结构

---
+++ src/net/include/net.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "net_err.h"
+
+net_err_t net_init(void);
+
+net_err_t net_start(void);
---
+++ src/net/include/net_err.h
@@ -0,0 +1,6 @@
+#pragma once
+
+typedef enum net_err_t
+{
+    NET_ERR_OK = 0
+} net_err_t;
